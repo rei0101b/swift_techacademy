d1c9b207562d650e691e50599a6ed801e756e3ce
Rei Yamaguchi <r-yamaguchi@froide.co.jp>
2017-06-09 18:17:50 +0900

    add: Pravte product

---
+++ SlideshowApp/FullScreenViewController.swift
@@ -0,0 +1,48 @@
+//
+//  FullScreenViewController.swift
+//  SlideshowApp
+//
+//  Created by FNDM04 on 2017/06/09.
+//  Copyright © 2017年 rei.yamaguchi. All rights reserved.
+//
+
+import UIKit
+
+class FullScreenViewController: UIViewController {
+    
+    var IMG:[UIImage] = []
+    var timeCountFromMain: Int = 0
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 写真の保存
+        for i in 1966...2017 {
+            let imgName: String = "IMG_" + String(i)
+            print(imgName)
+            IMG.append(UIImage(named: imgName)!)
+        }
+        imageView.image = IMG[timeCountFromMain]
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -7,19 +7,136 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
+    @IBOutlet weak var image: UIButton!
+    @IBOutlet weak var playStopButton: UIButton!
+    
+    var timer = Timer()
+    var playStopFlag = true
+    var IMG:[UIImage] = []
+    var timeCount = 0
+    var audioPlayer: AVAudioPlayer!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // 写真の保存
+        for i in 1966...2017 {
+            let imgName: String = "IMG_" + String(i)
+            print(imgName)
+            IMG.append(UIImage(named: imgName)!)
+        }
+        // 開くと同時にタイマー起動
+        lanchTimer()
+        // 音楽を流す
+        prepareMusic()
+        audioPlayer.play()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let fullScreenViewController: FullScreenViewController = segue.destination as! FullScreenViewController
+        fullScreenViewController.timeCountFromMain = timeCount - 1
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // timerを止める
+        playStopFlag = true
+        changePlayStop()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue){}
 
+    @IBAction func play(_ sender: Any) {
+        changePlayStop()
+    }
+    
+    @IBAction func next(_ sender: Any) {
+        // timerを止める
+        playStopFlag = true
+        changePlayStop()
+        if timeCount <= 50 {
+            timeCount += 1
+            image.setImage(IMG[timeCount], for: UIControlState())
+            return
+        }
+        timeCount = 0
+        image.setImage(IMG[timeCount], for: UIControlState())
+    }
+    @IBAction func back(_ sender: Any) {
+        // timerを止める
+        playStopFlag = true
+        changePlayStop()
+        if 0 < timeCount && timeCount <= 50 {
+            timeCount -= 1
+            image.setImage(IMG[timeCount], for: UIControlState())
+            return
+        }
+        timeCount = 50
+        image.setImage(IMG[timeCount], for: UIControlState())
+    }
+    
+    func slideImage() {
+        if timeCount <= 50 {
+            image.setImage(IMG[timeCount], for: UIControlState())
+            timeCount += 1
+            return
+        }
+        timeCount = 0
+    }
+    
+    // 「再生」を押すと「停止」
+    func changePlayStop() {
+        if playStopFlag == false {
+            playStopButton.setTitle("停止", for: .normal)
+            restartSlideShow()
+            playStopFlag = true
+            return
+        }
+        playStopButton.setTitle("再生", for: .normal)
+        stopSlideShow()
+        playStopFlag = false
+    }
+    
+    func restartSlideShow() {
+        lanchTimer()
+    }
+    
+    func stopSlideShow() {
+        timer.invalidate()
+    }
+    
+    func lanchTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(slideImage), userInfo: nil, repeats: true)
+    }
+    
+    func prepareMusic() {
+        // 再生する audio ファイルのパスを取得
+        let audioPath = Bundle.main.path(forResource: "ONE-OK-ROCK", ofType:"mp3")!
+        let audioUrl = URL(fileURLWithPath: audioPath)
+        // auido を再生するプレイヤーを作成する
+        var audioError:NSError?
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: audioUrl)
+        } catch let error as NSError {
+            audioError = error
+            audioPlayer = nil
+        }
+        // エラーが起きたとき
+        if let error = audioError {
+            print("Error \(error.localizedDescription)")
+        }
+        audioPlayer.delegate = self
+        audioPlayer.prepareToPlay()
+    }
+    
 
 }
 
