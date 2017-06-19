e8a4a2ae289dbf1c2fbe1a8ea62481770dbcb5c3
Rei Yamaguchi <r-yamaguchi@froide.co.jp>
2017-06-19 20:08:13 +0900

    作成途中

---
+++ instagram/LoginViewController.swift
@@ -0,0 +1,85 @@
+//
+//  LoginViewController.swift
+//  instagram
+//
+//  Created by FNDM04 on 2017/06/19.
+//  Copyright © 2017年 rei.yamaguchi. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mailAddressTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var displayNameTextField: UITextField!
+    
+    //　ログインボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handleLoginButton(_ sender: Any) {
+        
+    }
+    
+    // アカウント作成ボタンをタップしたときに呼ばれるメソッド
+    @IBAction func handleCreateAcountButton(_ sender: Any) {
+        if let address = mailAddressTextField.text, let password = passwordTextField.text, let displayName = displayNameTextField.text {
+            // アドレスとパスワード表示のいずれかでも入力されていないときは何もしない
+            if address.characters.isEmpty || password.characters.isEmpty || displayName.characters.isEmpty {
+                print("DEBUG_PRINT: 何かが空文字です")
+                return
+            }
+            
+            // アドレスとパスワードでユーザー作成.　ユーザー作成に成功すると自動的にログインする
+            FIRAuth.auth()?.createUser(withEmail: address, password: password) { user, error in
+                if let error = error {
+                  // エラーがあるとでバック
+                    print("DEBAG_PRINT: " + error.localizedDescription)
+                    return
+                }
+                print("DEBUG_PRINT: ユーザー作成に成功しました")
+                // 表示名を設定する
+                let user = FIRAuth.auth()?.currentUser
+                if let user = user {
+                    let changeRequest = user.profileChangeRequest()
+                    changeRequest.displayName = displayName
+                    changeRequest.commitChanges { error in
+                        if let error = error {
+                            print("DEBUG_PRINT: " + error.localizedDescription)
+                        }
+                        print("DEBUG_PRINT: [displayName = \(user.displayName!)]の設定に成功しました")
+                        // 画面を閉じてViewControllerに戻る
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                } else {
+                    print("DEBUG_PRINT: displayNameの設定に失敗しました")
+                }
+            }
+        }
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- instagram/ViewController.swift
+++ instagram/ViewController.swift
@@ -7,18 +7,64 @@
 //
 
 import UIKit
+import ESTabBarController
+import Firebase
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupTab()
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // currentUserがnilならログインしていない
+        if FIRAuth.auth()?.currentUser == nil {
+            // ログインしていなければ、ログイン画面を表示する
+            // viewDidAppear内でpresent()を呼び出しても表示されないためメソッドが終了してから呼ばれるようにする
+            DispatchQueue.main.async {
+                let loginViewController = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+                self.present(loginViewController!, animated: true, completion: nil)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func setupTab() {
+        // 画像のファイル名を指定してESTabBarControllerを作成する
+        let tabBarContoller: ESTabBarController! = ESTabBarController(tabIconNames: ["home", "camera", "setting"])
+        // 背景色、選択時の色を設定
+        tabBarContoller.selectedColor = UIColor(red: 1.0, green: 0.44, blue: 0.11, alpha: 1)
+        tabBarContoller.buttonsBackgroundColor = UIColor(red: 0.96, green: 0.91, blue: 0.87, alpha: 1)
+        // 作成したESTabBarControllerを親のViewContollerに追加
+        addChildViewController(tabBarContoller)
+        view.addSubview(tabBarContoller.view)
+        tabBarContoller.view.frame = view.bounds
+        tabBarContoller.didMove(toParentViewController: self)
+        // タブをタップしたときに表示するViewControllerを設定する
+        let homeViewController = storyboard?.instantiateViewController(withIdentifier: "Home")
+        let settingViewController = storyboard?.instantiateViewController(withIdentifier: "Setting")
+        
+        tabBarContoller.setView(homeViewController, at: 0)
+        tabBarContoller.setView(settingViewController, at: 2)
+        
+        // 真ん中のタブはボタンとして扱う
+        tabBarContoller.highlightButton(at: 1)
+        tabBarContoller.setAction({
+            // ボタンが押されたらImageViewControllerをモーダルで表示する
+            let imageViewController = self.storyboard?.instantiateViewController(withIdentifier: "ImageSelect")
+            self.present(imageViewController!, animated: true, completion: nil)
+        }, at: 1)
+        
+    }
+    
 
 
 }
